937b83d7fe86e642eb2b6ab2a09323de04315d5a
Zhiyao Wang <zwan0225@student.monash.edu>
2020-11-07 21:23:48 +1100

    添加项目文件。

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("624180819@qq.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
